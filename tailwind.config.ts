3061aefe137c681f3b6c9f5574c737a7d72b8f39
B1ckb0x <a.emmanuel0616@gmail.com>
2025-10-10 02:08:03 -0400

    fix: CSS not showing in prod

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,19 @@
-import { frostedThemePlugin } from "@whop/react/tailwind";
+// import { frostedThemePlugin } from "@whop/react/tailwind";
 
-export default { plugins: [frostedThemePlugin()] };
+// export default { plugins: [frostedThemePlugin()] };
+
+
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+export default config;
